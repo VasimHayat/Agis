5239e55b9bf4b001be4e6f1f5e3453e9382205ad
Vasim <vasimvasim.78639@gmail.com>
2020-01-08 23:24:47 +0530

    update
    update

--- dist/agis.js
+++ dist/agis.js
@@ -1,19 +1,65 @@
 var AgisLibrary = /** @class */ (function () {
     function AgisLibrary() {
         var _this = this;
+        this.init = function (selector) {
+            _this.isIdSelector = _this.isId(selector);
+            if (_this.isIdSelector) {
+                selector = selector.substr(1, selector.length);
+                _this.element = document.getElementById(selector);
+            }
+            else {
+                _this.elements = document.querySelectorAll(selector);
+            }
+            return _this;
+        };
         this.addClass = function (classList) {
-            _this._element.classList.add(classList);
+            if (_this.isIdSelector) {
+                _this.element.classList.add(classList);
+            }
+            else {
+                _this.elements.forEach(function (ele) {
+                    ele.classList.add(classList);
+                });
+            }
             return _this;
         };
         this.removeClass = function (classList) {
-            _this._element.classList.remove(classList);
+            if (_this.isIdSelector) {
+                _this.element.classList.remove(classList);
+            }
+            else {
+                _this.elements.forEach(function (ele) {
+                    ele.classList.remove(classList);
+                });
+            }
             return _this;
         };
-        this.init = function (id) {
-            _this._element = document.getElementById(id);
-            return _this;
+        this.hasClass = function (className) {
+            if (_this.isIdSelector) {
+                return _this.element.classList.contains(className);
+            }
+            else {
+                var nodeLen = _this.elements.length;
+                if (nodeLen == 0) {
+                    return false;
+                }
+                for (var i = 0; i < nodeLen; i++) {
+                    if (_this.elements[i].classList.contains(className)) {
+                        return true;
+                    }
+                    if (i == nodeLen - 1) {
+                        return false;
+                    }
+                }
+            }
         };
     }
+    AgisLibrary.prototype.startsWith = function (stringVal, key) {
+        return stringVal.substr(0, 1) == key;
+    };
+    AgisLibrary.prototype.isId = function (selector) {
+        return this.startsWith(selector, "#");
+    };
     return AgisLibrary;
 }());
 var Agis = new AgisLibrary().init;
--- src/agis.ts
+++ src/agis.ts
@@ -1,20 +1,77 @@
-class AgisLibrary {
-    private _element: (HTMLElement | null);
 
+
+class AgisLibrary {
+    public elements: (NodeListOf<HTMLElement> | null);
+
+    public element: (HTMLElement | null);
+
+    private isIdSelector: boolean;
+
+    startsWith(stringVal: string, key: string) {
+        return stringVal.substr(0, 1) == key;
+    }
+
+    private isId(selector: string) {
+        return this.startsWith(selector, "#");
+    }
+
+
+    init = (selector: string) => {
+        this.isIdSelector = this.isId(selector);
+        if (this.isIdSelector) {
+            selector = selector.substr(1, selector.length);
+            this.element = document.getElementById(selector);
+        } else {
+            this.elements = document.querySelectorAll(selector);
+        }
+        return this;
+    }
 
     addClass = (classList: string) => {
-        this._element.classList.add(classList);
+        if (this.isIdSelector) {
+            this.element.classList.add(classList);
+        } else {
+            this.elements.forEach((ele) => {
+                ele.classList.add(classList);
+            })
+        }
+
+
         return this;
     }
     removeClass = (classList: string) => {
-        this._element.classList.remove(classList);
+        if (this.isIdSelector) {
+            this.element.classList.remove(classList);
+        } else {
+            this.elements.forEach((ele) => {
+                ele.classList.remove(classList);
+            })
+        }
+
         return this;
+    }
+    hasClass = (className: string) => {
+        if (this.isIdSelector) {
+            return this.element.classList.contains(className);
+        } else {
+            const nodeLen = this.elements.length;
+            if (nodeLen == 0) { return false }
+            for (let i = 0; i < nodeLen; i++) {
+                if (this.elements[i].classList.contains(className)) {
+                    return true;
+                }
+                if (i == nodeLen - 1) {
+                    return false;
+                }
+            }
+
+
+        }
+
+
     }
 
-    init = (id: string) => {
-        this._element = document.getElementById(id);
-        return this;
-    }
+
 
 
 }
